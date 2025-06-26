c006b384dfc077fca29d2ce315827cd57a5ca79f
Jaime Muñoz <jaime.munozq@udea.edu.co>
2025-06-26 18:59:09 -0500

    Rename test method for login and add test method for adding a contact

--- src/test/java/org/udea/parabank/TestRunner.java
+++ src/test/java/org/udea/parabank/TestRunner.java
@@ -5,8 +5,15 @@ import com.intuit.karate.junit5.Karate;
 class TestRunner {
 
     @Karate.Test
-    Karate test01_ParabankLogin() {
+    Karate test01_AppContactLogin() {
         return Karate.run("login")
+                .relativeTo(getClass())
+                .outputCucumberJson(true);
+    }
+
+    @Karate.Test
+    Karate test02_AppContactAdd() {
+        return Karate.run("addContact")
                 .relativeTo(getClass())
                 .outputCucumberJson(true);
     }
